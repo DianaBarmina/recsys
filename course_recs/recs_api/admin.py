faa7836ba6f1fd7b6b097a66ec9fad325946c3dd
Diana <diana.barmina.2003@mail.ru>
2025-03-09 20:07:12 +0300

    Adding changes

--- course_recs/recs_api/admin.py
+++ course_recs/recs_api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserStudent, UserCourse, UserPlatform, Platform, Course
+from .models import UserStudent, UserCourse, UserPlatform, Platform, Course, Recommendations
 # Register your models here.
 
 admin.site.register(UserStudent)
@@ -7,3 +7,4 @@ admin.site.register(Platform)
 admin.site.register(Course)
 admin.site.register(UserPlatform)
 admin.site.register(UserCourse)
+admin.site.register(Recommendations)
--- course_recs/recs_api/urls.py
+++ course_recs/recs_api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from .views import UserRegisterView, custom_logout, HomeSortView, CourseDetailView, UserCoursesView, \
     UserCourseDeleteView, HomeSortView2, UserCourseCreateView,  CreateUserCourseView, CreateUserPlatformView, \
-    change_username, change_password
+    change_username, change_password, delete_course
 
 
 urlpatterns = [
@@ -12,7 +12,7 @@ urlpatterns = [
     path('logout/', custom_logout, name='logout'),
     path('course/<int:pk>', CourseDetailView.as_view(), name="course"),
     path('user/<int:user_id>', UserCoursesView, name="profile"),
-    path('user/<int:user_id>/<int:pk>/delete', UserCourseDeleteView.as_view(), name='delete-course'),
+    path('user/<int:user_id>/<int:pk>/delete', delete_course, name='delete-course'),
     #path('create/<int:course>/<int:user>/create', UserCourseCreateView.as_view(), name='create-usercourse'),
     #path('create/<int:course>/<int:user>/create', create_user_course, name='create-usercourse'),
     path('create/<int:course>/<int:user>/create', CreateUserCourseView.as_view(), name='create-usercourse'),
